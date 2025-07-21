f1cace9893e0769784c01e96d6b66a8febb0075d
newsoft111 <ydj0617@gmail.com>
2025-07-21 15:28:10 +0900

    Update profile.ts

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,6 +1,6 @@
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { IProfile } from '../component/profile/IProfile';
 import image from '../asset/sample_tux.png';
@@ -21,12 +21,13 @@ const profile: IProfile.Payload = {
       link: 'mailto:ydj0617@gmail.com',
       icon: faEnvelope,
     },
-    // {
-    //   id: 2,
-    //   title: 'Please contact me by email',
-    //   icon: faPhone,
-    //   badge: true,
-    // },
+    {
+      id: 2,
+      title: '+82 10-3633-9999',
+      link: 'tel:+821036339999',
+      icon: faPhone,
+      badge: true,
+    },
     {
       id: 3,
       link: 'https://github.com/newsoft111',
