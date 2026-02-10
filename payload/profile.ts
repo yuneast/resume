1ed23fd8286b261fd283831eae5613886cb3025b
Dongjun Yun <jun@ulim.co>
2026-02-10 10:33:30 +0900

    [DOCS] 이력서 기여도, 트래픽, 협업, 블로그 정보 보강

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,4 +1,4 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone, faBlog } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import { faBell } from '@fortawesome/free-regular-svg-icons';
@@ -31,6 +31,10 @@ const profile: IProfile.Payload = {
     {
       link: 'https://github.com/yuneast',
       icon: faGithub,
+    },
+    {
+      link: 'https://yuneast.github.io/',
+      icon: faBlog,
     },
   ],
   notice: {
